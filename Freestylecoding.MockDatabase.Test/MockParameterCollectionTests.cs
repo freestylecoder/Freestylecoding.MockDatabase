c55ec30e7c8140a65217485e679b80e5ed80cbad
Chris Gardner <cgardner@gmail.com>
2020-12-07 13:20:28 -0600

    Tests on the standend usage for indidivual classes.

--- Freestylecoding.MockDatabase.Test/MockParameterCollectionTests.cs
+++ Freestylecoding.MockDatabase.Test/MockParameterCollectionTests.cs
@@ -98,6 +98,378 @@ namespace Freestylecoding.MockDatabase.Test {
 		}
 		#endregion
 
+		#region bool Contains( string value )
+		[Fact]
+		public void Contains() {
+			const string input = nameof( this.Contains );
+			Collection.Add( CreateParameter( input, 42 ) );
+			Assert.True( this.Collection.Contains( input ) );
+		}
+
+		[Fact]
+		public void Contains_False() {
+			const string input = nameof( this.Contains_False );
+			Assert.False( this.Collection.Contains( input ) );
+		}
+
+		[Theory]
+		[InlineData( "prm" )]
+		[InlineData( "prM" )]
+		[InlineData( "pRm" )]
+		[InlineData( "pRM" )]
+		[InlineData( "Prm" )]
+		[InlineData( "PrM" )]
+		[InlineData( "PRm" )]
+		[InlineData( "PRM" )]
+		public void Contains_CaseInsensitive( string name ) {
+			Collection.Add( CreateParameter( "prm", 42 ) );
+			Assert.True( this.Collection.Contains( name ) );
+		}
+
+		[Theory]
+		[InlineData( "" )]
+		[InlineData( " " )]
+		[InlineData( "\t" )]
+		[InlineData( null )]
+		public void Contains_NullOrWhiteSpace( string name ) {
+			Assert.False( this.Collection.Contains( name ) );
+		}
+		#endregion
+
+		#region void CopyTo( Array array, int index )
+		[Fact]
+		public void CopyTo() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			MockParameter[] actual = new MockParameter[5];
+
+			Collection.AddRange( original );
+			Collection.CopyTo( actual, 1 );
+
+			Assert.Null( actual[0] );
+			Assert.Equal( original, actual.Skip(1) );
+		}
+
+		[Fact]
+		public void CopyTo_BadIndex() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			MockParameter[] actual = new MockParameter[4];
+
+			Collection.AddRange( original );
+			Assert.IsType<ArgumentOutOfRangeException>(
+				Record.Exception( () =>
+					Collection.CopyTo( actual, -1 )
+				)
+			);
+		}
+
+		[Fact]
+		public void CopyTo_NotLongEnough() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			MockParameter[] actual = new MockParameter[4];
+
+			Collection.AddRange( original );
+			Assert.IsType<ArgumentException>(
+				Record.Exception( () =>
+					Collection.CopyTo( actual, 1 )
+				)
+			);
+		}
+
+		[Fact]
+		public void CopyTo_WrongArrayType() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			int[] actual = new int[5];
+
+			Collection.AddRange( original );
+			Assert.IsType<ArgumentException>(
+				Record.Exception( () =>
+					Collection.CopyTo( actual, 1 )
+				)
+			);
+		}
+		#endregion
+
+		#region int IndexOf( object value )
+		[Fact]
+		public void IndexOf_Object() {
+			MockParameter[] original = Enumerable.Range( 1, 3 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original );
+
+			Assert.Equal( 1, Collection.IndexOf( Collection[1] ) );
+		}
+
+		[Fact]
+		public void IndexOf_Object_NotFound() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original.Take( 3 ).ToArray() );
+
+			Assert.Equal( -1, Collection.IndexOf( original[3] ) );
+		}
+
+		[Fact]
+		public void IndexOf_Object_Null() {
+			Assert.Equal( -1, Collection.IndexOf( null ) );
+		}
+
+		[Fact]
+		public void IndexOf_Object_BadType() {
+			Assert.IsType<InvalidCastException>( 
+				Record.Exception( () =>
+					Collection.IndexOf( new object() )
+				)
+			);
+		}
+		#endregion
+
+		#region bool IndexOf( string parameterName )
+		[Fact]
+		public void IndexOf() {
+			const string input = nameof( this.IndexOf );
+			Collection.Add( CreateParameter( input, 42 ) );
+			Collection.Add( CreateParameter( $"Not{input}", 666 ) );
+			Assert.Equal( 0, this.Collection.IndexOf( input ) );
+		}
+
+		[Fact]
+		public void IndexOf_False() {
+			const string input = nameof( this.IndexOf_False );
+			Assert.Equal( -1, this.Collection.IndexOf( input ) );
+		}
+
+		[Fact]
+		public void IndexOf_FindsFirst() {
+			const string input = nameof( this.IndexOf );
+			Collection.Add( CreateParameter( input, 42 ) );
+			Collection.Add( CreateParameter( input, 666 ) );
+			Assert.Equal( 0, this.Collection.IndexOf( input ) );
+		}
+
+		[Theory]
+		[InlineData( "prm" )]
+		[InlineData( "prM" )]
+		[InlineData( "pRm" )]
+		[InlineData( "pRM" )]
+		[InlineData( "Prm" )]
+		[InlineData( "PrM" )]
+		[InlineData( "PRm" )]
+		[InlineData( "PRM" )]
+		public void IndexOf_CaseInsensitive( string name ) {
+			Collection.Add( CreateParameter( "Notprm", 666 ) );
+			Collection.Add( CreateParameter( "prm", 42 ) );
+			Assert.Equal( 1, this.Collection.IndexOf( name ) );
+		}
+
+		[Theory]
+		[InlineData( "" )]
+		[InlineData( " " )]
+		[InlineData( "\t" )]
+		[InlineData( null )]
+		public void IndexOf_NullOrWhiteSpace( string name ) {
+			Assert.Equal( -1, this.Collection.IndexOf( name ) );
+		}
+		#endregion
+
+		#region void Insert( int index, object value )
+		[Fact]
+		public void Insert() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original.Skip( 2 ).Prepend( original[0] ).ToArray() );
+			Collection.Insert( 1, original[1] );
+
+			Assert.Equal( original, Collection.OfType<MockParameter>() );
+		}
+
+		[Fact]
+		public void Insert_Prepend() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original.Skip( 1 ).ToArray() );
+			Collection.Insert( 0, original[0] );
+
+			Assert.Equal( original, Collection.OfType<MockParameter>() );
+		}
+
+		[Fact]
+		public void Insert_Append() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original.Take( 3 ).ToArray() );
+			Collection.Insert( 3, original[3] );
+
+			Assert.Equal( original, Collection.OfType<MockParameter>() );
+		}
+
+		[Theory]
+		[InlineData( -1 )]
+		[InlineData( 10 )]
+		public void Insert_OutOfRange( int index ) {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original.Skip( 2 ).Prepend( original[0] ).ToArray() );
+			Assert.IsType<ArgumentOutOfRangeException>(
+				Record.Exception( () =>
+					Collection.Insert( index, original[1] )
+				)
+			);
+		}
+
+		[Fact]
+		public void Insert_Duplicate() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original );
+			Assert.IsType<ArgumentException>(
+				Record.Exception( () =>
+					Collection.Insert( 4, original[3] )
+				)
+			);
+		}
+
+		[Fact]
+		public void Insert_Null() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original );
+			Assert.IsType<ArgumentNullException>(
+				Record.Exception( () =>
+					Collection.Insert( 4, null )
+				)
+			);
+		}
+
+		[Fact]
+		public void Insert_WrongType() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original );
+			Assert.IsType<InvalidCastException>(
+				Record.Exception( () =>
+					Collection.Insert( 4, new object() )
+				)
+			);
+		}
+		#endregion
+
+		#region void Remove( object value )
+		[Fact]
+		public void Remove() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original );
+			Collection.Remove( original[0] );
+
+			Assert.Equal( original.Skip( 1 ), Collection.OfType<MockParameter>() );
+		}
+
+		[Fact]
+		public void Remove_NotFound() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original.Take( 3 ).ToArray() );
+			Collection.Remove( original[3] );
+
+			Assert.Equal( original.Take( 3 ), Collection.OfType<MockParameter>() );
+		}
+
+		[Fact]
+		public void Remove_Null() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original );
+			Assert.IsType<ArgumentNullException>( 
+				Record.Exception( () =>
+					Collection.Remove( null )
+				)
+			);
+		}
+
+		[Fact]
+		public void Remove_WrongType() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original );
+			Assert.IsType<InvalidCastException>( 
+				Record.Exception( () =>
+					Collection.Remove( new object() )
+				)
+			);
+		}
+		#endregion
+
+		#region void RemoveAt( string parameterName )
+		[Fact]
+		public void RemoveAt() {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original );
+			Collection.RemoveAt( original[0].ParameterName );
+
+			Assert.Equal( original.Skip( 1 ), Collection.OfType<MockParameter>() );
+		}
+
+		[Theory]
+		[InlineData( "" )]
+		[InlineData( " " )]
+		[InlineData( "\t" )]
+		[InlineData( null )]
+		[InlineData( "BadParam" )]
+		public void RemoveAt_NotFound( string name ) {
+			MockParameter[] original = Enumerable.Range( 1, 4 )
+				.Select( i => CreateParameter( $"Param{i}", i ) )
+				.ToArray();
+
+			Collection.AddRange( original );
+			Assert.IsType<IndexOutOfRangeException>(
+				Record.Exception( () =>
+					Collection.RemoveAt( name )
+				)
+			);
+		}
+		#endregion
+
 		private static Random random = new Random( Guid.NewGuid().GetHashCode() );
 		private static T GetRandomEnum<T>() {
 			Array values = Enum.GetValues( typeof( T ) );
--- Freestylecoding.MockDatabase/MockParameterCollection.cs
+++ Freestylecoding.MockDatabase/MockParameterCollection.cs
@@ -40,28 +40,56 @@ namespace Freestylecoding.MockDatabase {
 		public override bool Contains( object value ) =>
 			list.Contains( value );
 		public override bool Contains( string value ) =>
-			//TODO: Need to see if parameter name is case insensitive
-			list.Any( p => p.ParameterName.Equals( value ) );
-		public override void CopyTo( Array array, int index ) =>
-			list.CopyTo( array.OfType<DbParameter>().ToArray(), index );
+			list.Any( p => p.ParameterName.Equals( value, StringComparison.InvariantCultureIgnoreCase ) );
+		public override void CopyTo( Array array, int index ) {
+			if( !typeof( DbParameter ).IsAssignableFrom( array.GetType().GetElementType() ) )
+				throw new ArgumentException( "Target array type is not compatible with the type of items in the collection." );
+
+			list.CopyTo( array as DbParameter[], index );
+		}
 		public override IEnumerator GetEnumerator() =>
 			list.GetEnumerator();
-		public override int IndexOf( object value ) =>
-			list.IndexOf( value as DbParameter );
-		public override int IndexOf( string parameterName ) =>
-			//TODO: Need to see if parameter name is case insensitive
-			IndexOf( list.FirstOrDefault( p => p.ParameterName.Equals( parameterName ) ) );
+		public override int IndexOf( object value ) {
+			if( null != value )
+				if( !typeof( DbParameter ).IsAssignableFrom( value.GetType() ) )
+					throw new InvalidCastException( "The DbParameterCollection only accepts non-null DbParameter type objects, not Object objects." );
 
-		public override void Insert( int index, object value ) =>
+			return list.IndexOf( value as DbParameter );
+		}
+		public override int IndexOf( string parameterName ) =>
+			IndexOf( list.FirstOrDefault( p => p.ParameterName.Equals( parameterName, StringComparison.InvariantCultureIgnoreCase ) ) );
+
+		public override void Insert( int index, object value ) {
+			if( null == value )
+				throw new ArgumentNullException( "value", "The DbParameterCollection only accepts non-null DbParameter type objects." );
+
+			if( !typeof( DbParameter ).IsAssignableFrom( value.GetType() ) )
+				throw new InvalidCastException( "The DbParameterCollection only accepts non-null DbParameter type objects, not Object objects." );
+
+			if( list.Contains( value ) )
+				throw new ArgumentException( "The DbParameter is already contained by another DbParameterCollection." );
+
 			list.Insert( index, value as DbParameter );
-		public override void Remove( object value ) =>
+		}
+		public override void Remove( object value ) {
+			if( null == value )
+				throw new ArgumentNullException( "value", "The DbParameterCollection only accepts non-null DbParameter type objects." );
+
+			if( !typeof( DbParameter ).IsAssignableFrom( value.GetType() ) )
+				throw new InvalidCastException( "The DbParameterCollection only accepts non-null DbParameter type objects, not Object objects." );
+
 			list.Remove( value as DbParameter );
-		public override void RemoveAt( int index ) =>
+		}
+		public override void RemoveAt( int index ) {
+			if( index < 0 || index >= list.Count )
+				throw new IndexOutOfRangeException();
+
 			list.RemoveAt( index );
+		}
 
 		// These are the ones that needed special treatment
 		public override void RemoveAt( string parameterName ) =>
-			list.RemoveAt( this.IndexOf( parameterName ) );
+			this.RemoveAt( this.IndexOf( parameterName ) );
 		protected override DbParameter GetParameter( int index ) =>
 			list[index];
 		protected override DbParameter GetParameter( string parameterName ) =>
